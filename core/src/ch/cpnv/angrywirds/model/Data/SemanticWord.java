b72243079a7623e56e99b8495839271a66635cb1
Kevin <kevin.pasteur@cpnv.ch>
2020-11-12 09:31:12 +0100

    Update functions addTranslation() and getValue()

--- core/src/ch/cpnv/angrywirds/model/Data/SemanticWord.java
+++ core/src/ch/cpnv/angrywirds/model/Data/SemanticWord.java
@@ -2,14 +2,13 @@ package ch.cpnv.angrywirds.model.Data;
 import java.util.HashMap;
 
 public class SemanticWord {
-    private HashMap<String,String> values;
+    private HashMap<String,String> values = new HashMap<String, String>();
 
     public void addTranslation(String language, String value) {
-
+        this.values.put("language","value");
     }
 
     public String getValue(String language) {
-
-        return "";
+        return this.values.get("language");
     }
 }
