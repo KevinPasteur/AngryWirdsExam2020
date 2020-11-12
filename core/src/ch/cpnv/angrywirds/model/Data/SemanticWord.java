ec932c09651e0508d2058363360b4280bceab60d
Kevin <kevin.pasteur@cpnv.ch>
2020-11-12 09:55:13 +0100

    update SemanticWord for dynamic choose

--- core/src/ch/cpnv/angrywirds/model/Data/SemanticWord.java
+++ core/src/ch/cpnv/angrywirds/model/Data/SemanticWord.java
@@ -1,14 +1,16 @@
 package ch.cpnv.angrywirds.model.Data;
+import com.badlogic.gdx.Gdx;
+
 import java.util.HashMap;
 
 public class SemanticWord {
     private HashMap<String,String> values = new HashMap<String, String>();
 
     public void addTranslation(String language, String value) {
-        this.values.put("language","value");
+        this.values.put(language,value);
     }
 
     public String getValue(String language) {
-        return this.values.get("language");
+        return this.values.get(language);
     }
 }
