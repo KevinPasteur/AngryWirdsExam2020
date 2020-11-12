b6ce8b37e331878edcac830e358040f7d99effcc
Kevin <kevin.pasteur@cpnv.ch>
2020-11-12 09:55:32 +0100

    Update name, width and height

--- core/src/ch/cpnv/angrywirds/model/Button.java
+++ core/src/ch/cpnv/angrywirds/model/Button.java
@@ -4,10 +4,12 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Button extends TextualObject {
     private String value;
-
+    public static final int WIDTH = 300/2;
+    public static final int HEIGHT = 298 / 3;
+    private static final String SPRITE_NAME = "rubber.png";
 
     public Button(Vector2 point, String value){
-        super(new Vector2(point.x,point.y),50,50,"",value);
+        super(new Vector2(point.x,point.y),WIDTH,HEIGHT,SPRITE_NAME,value);
 
     }
 
