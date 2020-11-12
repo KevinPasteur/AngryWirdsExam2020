440f7c03d6e481724caa2b348b67f307a4c8ad80
Kevin <kevin.pasteur@cpnv.ch>
2020-11-12 09:20:28 +0100

    Create class SemanticWord

--- core/src/ch/cpnv/angrywirds/model/Data/SemanticWord.java
+++ core/src/ch/cpnv/angrywirds/model/Data/SemanticWord.java
@@ -1,5 +1,15 @@
 package ch.cpnv.angrywirds.model.Data;
+import java.util.HashMap;
 
 public class SemanticWord {
+    private HashMap<String,String> values;
 
+    public void addTranslation(String language, String value) {
+
+    }
+
+    public String getValue(String language) {
+
+        return "";
+    }
 }
